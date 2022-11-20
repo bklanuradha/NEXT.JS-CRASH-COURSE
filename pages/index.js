a1ce4022c30c0d234eab1a0bb06aa99b6ac09837
bklanuradha <92729@fhss.sjp.ac.lk>
2022-11-20 17:14:26 +0000

    Initial-Commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WebDev News</title>
+        <meta name='keywords' content='web development, programming'/>
+      </Head>
+      <h1>Welcome To Next</h1>
+    </div>
+  )
+}
